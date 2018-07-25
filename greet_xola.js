2ee73127fb2d93b6383ba49743f6a53c8cf1be55
ayolicias <ziyandandlamafa47@gmail.com>
2018-07-25 11:18:29 +0200

    added changes

---
+++ greet_xola.js
@@ -0,0 +1,35 @@
+
+const chalk = require('chalk');
+//import the greet module that is in the current folder
+const greet = require('./greet');
+
+const styledMessage = chalk.bgGreen.black(greet('Xola'));
+console.log(styledMessage)
+
+var figlet = require('figlet');
+
+figlet('Hello World!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
+figlet.text('Boo!', {
+    font: 'Ghost',
+    horizontalLayout: 'default',
+    verticalLayout: 'default'
+}, function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
+console.log(figlet.textSync('Boo!', {
+    font: 'Ghost',
+    horizontalLayout: 'default',
+    verticalLayout: 'default'
+}));
